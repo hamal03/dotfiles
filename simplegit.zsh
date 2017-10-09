5c0aaf23ace91f437ff23ee848538cc18bc9d6c0
Rob S. Wolfram <propdf@hamal.nl>
2017-10-09 09:57:44 +0200

    simplegit: X>< and documentation

--- simplegit.zsh
+++ simplegit.zsh
@@ -3,6 +3,28 @@
 # (c) Rob Wolfram <propdf@hamal.nl>
 # Added to public domain.
 
+# Legend:
+# (branch|flags)
+# branch can be followed by "<" (you're behind remote origin), ">" (you're
+# ahead of remote origin) or both.
+# Flags:
+# .    nothing to commit
+# X    there are conflicts
+# +    files are staged
+# *    files are modified
+# ?    untracked files are present
+
+# Usage:
+# ZSH: include the following in your .zshrc file (the ellipses denote
+# your own part of PROMPT):
+#  setopt PROMPT_SUBST
+#  source /path/to/simplegit.zsh
+#  export PROMPT='...$(__simplegit)...'
+# BASH: include the following in your .bashrc (the ellipses denote
+# your own part of PS1):
+#  source /path/to/simplegit.bash
+#  PS1='...$(__simplegit)...'
+
 __simplegit() {
     (
         declare -a __mystatlines
@@ -11,15 +33,19 @@ __simplegit() {
         __mystat=$(git status --porcelain 2>/dev/null)
         [ -n "$ZSH_NAME" ] && __mystatlines=( ${(f)__mystat} )
         [ -n "$BASH" ] && readarray -t __mystatlines <<< "$__mystat"
-        __sgadd=""
-        __sgmod=""
-        __sgunk=""
         for line in ${__mystatlines[@]} ; do
+            [ "${line:0:1}" = "U" ] && __sgbrk="X"
             [ "${line:0:1}" = "A" -o "${line:0:1}" = "M" ] && __sgadd="+"
             [ "${line:1:1}" = "M" ] && __sgmod="*"
             [ "${line:1:1}" = "?" ] && __sgunk="?"
-            [ -n "$__sgadd" -a -n "$__sgmod" -a -n "$__sgunk" ] && break
         done
-        echo "(${__mybr}|${__sgadd}${__sgmod}${__sgunk})"
+        if [ -z "${__sgbrk}${__sgadd}${__sgmod}${__sgunk}" ] ; then
+            __sgbrk="."
+            __mystat=$(git status 2>/dev/null)
+            [[ "$__mystat" =~ Your.branch.*behind ]] && __rem="<"
+            [[ "$__mystat" =~ Your.branch.*ahead ]] && __rem=">"
+            [[ "$__mystat" =~ Your.branch.*diverged ]] && __rem="<>"
+        fi
+        echo "(${__mybr}${__rem}|${__sgbrk}${__sgadd}${__sgmod}${__sgunk})"
     )
 }
